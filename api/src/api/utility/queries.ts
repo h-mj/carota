3eca81683d6977b7e57712a667021ae096dc9ada
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-06-17 20:00:56 +0300

    Remove debug console.log calls

--- api/src/api/utility/queries.ts
+++ api/src/api/utility/queries.ts
@@ -4,9 +4,6 @@ export const callCatch = async <T>(fn: () => Promise<T>): Promise<T> => {
   try {
     return await fn();
   } catch (error) {
-    console.log("XD");
-    console.log(typeof error.detail);
-
     if (error.code === "23505" /* unique constraint */) {
       // error detail should look like `Key ({field})=({value}) already exists.`
       // and we want to get `{field}` value.

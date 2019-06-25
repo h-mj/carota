e431615b3590190204d540fffc20c31d9fafbee6
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-06-25 23:29:48 +0300

    Replace loading circles with rounded squares

--- web/src/component/Loader.tsx
+++ web/src/component/Loader.tsx
@@ -1,16 +1,10 @@
 import { observable, action } from "mobx";
 import { observer } from "mobx-react";
 import * as React from "react";
-import styled, { keyframes } from "styled-components";
-import {
-  fadeIn,
-  fadeOut,
-  scaleIn,
-  scaleOut,
-  TRANSITION_DURATION
-} from "../styling/animations";
-import { ACTIVE, BACKGROUND_TRANSLUCENT } from "../styling/colors";
-import { UNIT } from "../styling/sizes";
+import styled from "styled-components";
+import { Loading } from "./icon/Loading";
+import { fadeIn, fadeOut, TRANSITION_DURATION } from "../styling/animations";
+import { BACKGROUND_TRANSLUCENT } from "../styling/colors";
 
 /**
  * Loader component props.
@@ -58,12 +52,7 @@ export class Loader extends React.Component<LoaderProps> {
 
     return (
       <Overlay isActive={loading}>
-        <DiskContainer>
-          <Disk />
-          <Disk />
-          <Disk />
-          <Disk />
-        </DiskContainer>
+        <Loading />
       </Overlay>
     );
   }
@@ -111,64 +100,4 @@ const Overlay = styled.div<OverlayProps>`
   /* Don't let users select components below the overlay */
   user-select: ${props => (props.isActive ? "auto" : "none")};
   pointer-events: ${props => (props.isActive ? "initial" : "none")};
-`;
-
-/**
- * Diameter of disk in `rem`s.
- */
-const DISK_SIZE = UNIT / 8;
-
-/**
- * Offset between the start position of one disk and next one.
- */
-const DISK_OFFSET = UNIT / 4;
-
-/**
- * Component that contains moving disks.
- */
-const DiskContainer = styled.div`
-  position: relative;
-  width: ${2 * DISK_OFFSET + DISK_SIZE}rem;
-  height: ${DISK_SIZE}rem;
-`;
-
-/**
- * Animation that moves a disk to the right by `DISK_OFFSET`.
- */
-const move = keyframes`
-  from { transform: translateX(0); }
-  to { transform: translateX(${DISK_OFFSET}rem); }
-`;
-
-/**
- * One of the four animated disks the loader component contains.
- */
-const Disk = styled.div`
-  position: absolute;
-
-  width: ${DISK_SIZE}rem;
-  height: ${DISK_SIZE}rem;
-
-  border-radius: 50%;
-  background-color: ${ACTIVE};
-
-  animation-timing-function: cubic-bezier(0, 1, 1, 0);
-
-  &:nth-child(1) {
-    animation: ${scaleIn} 0.5s infinite;
-  }
-
-  &:nth-child(2) {
-    animation: ${move} 0.5s infinite;
-  }
-
-  &:nth-child(3) {
-    left: ${DISK_OFFSET}rem;
-    animation: ${move} 0.5s infinite;
-  }
-
-  &:nth-child(4) {
-    left: ${2 * DISK_OFFSET}rem;
-    animation: ${scaleOut} 0.5s infinite;
-  }
 `;
---
+++ web/src/component/icon/Loading.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import styled, { keyframes } from "styled-components";
+import { BORDER_RADIUS, UNIT } from "../../styling/sizes";
+import { ACTIVE, BACKGROUND } from "../../styling/colors";
+import { scaleIn, scaleOut } from "../../styling/animations";
+
+export const Loading: React.FunctionComponent = () => {
+  return (
+    <Squares>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </Squares>
+  );
+};
+
+/**
+ * Diameter of square in `rem`s.
+ */
+const SQUARE_SIZE = UNIT / 8;
+
+/**
+ * Offset between the start position of one square and next one.
+ */
+const SQUARE_OFFSET = UNIT / 4;
+
+/**
+ * Component that contains moving squares.
+ */
+export const Squares = styled.div`
+  position: relative;
+  width: ${2 * SQUARE_OFFSET + SQUARE_SIZE}rem;
+  height: ${SQUARE_SIZE}rem;
+`;
+
+/**
+ * Animation that moves a square to the right by `SQUARE_OFFSET`.
+ */
+const move = keyframes`
+  from { transform: translateX(0); }
+  to { transform: translateX(${SQUARE_OFFSET}rem); }
+`;
+
+/**
+ * One of the four animated squares the loading icon contains.
+ */
+const Square = styled.div`
+  position: absolute;
+
+  width: ${SQUARE_SIZE}rem;
+  height: ${SQUARE_SIZE}rem;
+
+  background-color: ${BACKGROUND};
+
+  border-radius: ${BORDER_RADIUS}rem;
+  box-shadow: 0 0 0 1px ${ACTIVE}, inset 0 0 0 1px ${ACTIVE};
+
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+
+  &:nth-child(1) {
+    animation: ${scaleIn} 0.5s infinite;
+  }
+
+  &:nth-child(2) {
+    left: ${2 * SQUARE_OFFSET}rem;
+    animation: ${scaleOut} 0.5s infinite;
+  }
+
+  &:nth-child(3) {
+    animation: ${move} 0.5s infinite;
+  }
+
+  &:nth-child(4) {
+    left: ${SQUARE_OFFSET}rem;
+    animation: ${move} 0.5s infinite;
+  }
+`;

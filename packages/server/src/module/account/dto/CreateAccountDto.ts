c715d1516fba7b91641c83ffda212a9ffe17f7ac
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-10-14 12:26:07 +0300

    fix: require birthDate to be before this instant

--- packages/server/src/module/account/dto/CreateAccountDto.ts
+++ packages/server/src/module/account/dto/CreateAccountDto.ts
@@ -1,12 +1,25 @@
-import { deviate, Success } from "deviator";
+import { deviate, err, ok, Success } from "deviator";
+import { DateTime } from "luxon";
 
 import { LANGUAGES, SEXES } from "../Account";
+
+const dateBeforeNow = (input: string) => {
+  const date = DateTime.fromISO(input);
+
+  if (!date.isValid) {
+    return err("invalidDate");
+  }
+
+  return date.diffNow("milliseconds").milliseconds <= 0
+    ? ok(date.toISODate())
+    : err("futureDate");
+};
 
 // prettier-ignore
 export const createAccountDtoValidator = deviate().object().shape({
   name: deviate().string().trim().notEmpty(),
   sex: deviate().string().options(SEXES),
-  birthDate: deviate().string(),
+  birthDate: deviate().string().append(dateBeforeNow),
   language: deviate().string().options(LANGUAGES),
   email: deviate().string().trim().notEmpty().lowercase().email(),
   password: deviate().string().notEmpty().minLen(8),

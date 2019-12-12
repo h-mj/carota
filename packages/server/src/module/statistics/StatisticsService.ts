1eb6e0f6ab38010a7c9a1cafe303f5aed6a073b0
Henri-Martin Jaakson <h.m.jaakson@gmail.com>
2019-12-12 20:45:46 +0200

    performance: reduce statistics response object size by not including not required fields

--- packages/server/src/module/statistics/StatisticsService.ts
+++ packages/server/src/module/statistics/StatisticsService.ts
@@ -5,31 +5,38 @@ import { Injectable } from "@nestjs/common";
 import { InvalidIdError } from "../../base/error/InvalidIdError";
 import { Account } from "../account/Account";
 import { AccountRepository } from "../account/AccountRepository";
-import {
-  RequiredNutrient,
-  REQUIRED_NUTRIENTS
-} from "../foodstuff/NutritionDeclaration";
+import { RequiredNutrient } from "../foodstuff/NutritionDeclaration";
 import { authorize as authorizeMeal } from "../meal/Meal";
 import { MealRepository } from "../meal/MealRepository";
 import {
-  authorize as authorizeMeasurement,
-  MeasurementDto,
+  QUANTITIES,
   Quantity,
-  QUANTITIES
+  authorize as authorizeMeasurement
 } from "../measurement/Measurement";
 import { MeasurementRepository } from "../measurement/MeasurementRepository";
 import { GetAllStatisticsDto } from "./dto/GetAllStatisticsDto";
 
+interface NutritionDataPoint {
+  date: string;
+  value: number;
+  limit: number;
+}
+
 interface NutritionDataSet {
   type: "nutrition";
   name: RequiredNutrient;
-  data: Array<{ date: string; value: number; limit: number }>;
+  data: NutritionDataPoint[];
+}
+
+interface QuantityDataPoint {
+  date: string;
+  value: number;
 }
 
 interface QuantityDataSet {
   type: "quantity";
   name: Quantity;
-  data: MeasurementDto[];
+  data: QuantityDataPoint[];
 }
 
 @Injectable()
@@ -55,44 +62,38 @@ export class StatisticsService {
     await authorizeMeasurement(principal, "get quantity measurements of", account);
     await authorizeMeal(principal, "get all meals of", account);
 
-    const meals = await Promise.all(
-      (
-        await mealRepository!.find({
-          where: { accountId: account.id },
-          relations: ["dishes", "dishes.foodstuff"]
-        })
-      ).map(meal => meal.toDto(principal))
+    const meals = await mealRepository!.find({
+      where: { accountId: account.id },
+      relations: ["dishes", "dishes.foodstuff"]
+    });
+
+    const mealDtos = await Promise.all(
+      meals.map(meal => meal.toDto(principal))
     );
 
-    const nutrientCounters = new Map<RequiredNutrient, Map<string, number>>([
+    const counters = new Map<RequiredNutrient, Map<string, number>>([
       ["energy", new Map()],
       ["protein", new Map()],
       ["fat", new Map()],
       ["carbohydrate", new Map()]
     ]);
 
-    for (const meal of meals) {
-      for (const nutrient of REQUIRED_NUTRIENTS) {
-        const nutrientCounter = nutrientCounters.get(nutrient)!;
-
-        nutrientCounter.set(
-          meal.date,
-          (nutrientCounter.get(meal.date) || 0) +
-            meal.dishes.reduce(
-              (sum, dish) =>
-                sum +
-                dish.quantity * dish.foodstuff.nutritionDeclaration[nutrient],
-              0
-            )
+    for (const [nutrient, counter] of counters.entries()) {
+      for (const dto of mealDtos) {
+        const nutrientAmount = dto.dishes.reduce(
+          (sum, dish) =>
+            sum + dish.quantity * dish.foodstuff.nutritionDeclaration[nutrient],
+          0
         );
+
+        counter.set(dto.date, (counter.get(dto.date) ?? 0) + nutrientAmount);
       }
     }
 
     const nutritionDataSet: NutritionDataSet[] = [];
 
-    for (const nutrient of REQUIRED_NUTRIENTS) {
-      const values = nutrientCounters.get(nutrient)!;
-      const counts = Array.from(values.entries()).sort(
+    for (const [nutrient, counter] of counters.entries()) {
+      const points = Array.from(counter.entries()).sort(
         (entry1, entry2) =>
           new Date(entry1[0]).valueOf() - new Date(entry2[0]).valueOf()
       );
@@ -100,7 +101,7 @@ export class StatisticsService {
       nutritionDataSet.push({
         type: "nutrition",
         name: nutrient,
-        data: counts.map(([date, value]) => ({
+        data: points.map(([date, value]) => ({
           date,
           value,
           limit: value - 10
@@ -108,13 +109,13 @@ export class StatisticsService {
       });
     }
 
-    const measurements = await Promise.all(
-      (
-        await measurementRepository!.find({
-          where: { accountId: account.id },
-          order: { date: "ASC" }
-        })
-      ).map(measurement => measurement.toDto())
+    const measurements = await measurementRepository!.find({
+      where: { accountId: account.id },
+      order: { date: "ASC" }
+    });
+
+    const measurementDtos = await Promise.all(
+      measurements.map(measurement => measurement.toDto())
     );
 
     const quantityDataSet: QuantityDataSet[] = QUANTITIES.map(quantity => ({
@@ -123,10 +124,11 @@ export class StatisticsService {
       data: []
     }));
 
-    for (const measurement of measurements) {
-      quantityDataSet[QUANTITIES.indexOf(measurement.quantity)].data.push(
-        measurement
-      );
+    for (const dto of measurementDtos) {
+      quantityDataSet[QUANTITIES.indexOf(dto.quantity)].data.push({
+        date: dto.date,
+        value: dto.value
+      });
     }
 
     return ([] as Array<NutritionDataSet | QuantityDataSet>).concat(
